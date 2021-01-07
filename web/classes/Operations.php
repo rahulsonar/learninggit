b29f6104a18668a6fa49cbe38eeddcc3b2bfbc81
Rahul Sonar <sonar.soni@gmail.com>
2021-01-07 19:26:52 +0530

    comments

--- web/classes/Operations.php
+++ web/classes/Operations.php
@@ -1,8 +1,32 @@
 <?php
 
-class Operations {
-	
-	function add($a,$b) {
-		return ($a+$b);
-	}
+/**
+ * Operations.php
+ * php version 7.4
+ 
+ * @Description Operations class
+ * @category    Class
+ * @package     Learninggit
+ * @author      My Name <my.name@example.com>
+ * @license     http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link        https://rahulsonar.com
+ */
+
+class Operations
+{
+    /**
+     * Add function
+     *
+     * @param int $a 
+     *               first
+     * @param int $b 
+     *               second number
+     * 
+     * @return int $c 
+     **/
+    function add($a,$b)
+    {
+        $c = ($a+$b);
+        return $c;
+    }
 }
