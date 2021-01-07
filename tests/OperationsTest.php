9cc17736e33398c9410406c3d40bed39e3eb6190
Rahul Sonar <sonar.soni@gmail.com>
2021-01-07 20:05:22 +0530

    fixes

--- tests/OperationsTest.php
+++ tests/OperationsTest.php
@@ -6,7 +6,7 @@ final class OperationsTest extends TestCase
 {
     public function testAdd():void {
 		$operations = new learninggit\Operations();
-		$this->assertEquals($_ENV['DEP_KEY'],"MY_DEPLOYMENT_KEY_123456");
+		$this->assertEquals($_SERVER['DEP_KEY'],"MY_DEPLOYMENT_KEY_123456");
 		$this->assertEquals(4,$operations->add(1,3));
 	}
 }
