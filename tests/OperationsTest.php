869437c6aad8101d50a32a1f5b23c0919bb343e4
Rahul Sonar <sonar.soni@gmail.com>
2021-01-07 19:57:10 +0530

    fixes

--- tests/OperationsTest.php
+++ tests/OperationsTest.php
@@ -1,10 +1,11 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
+use learninggit;
 
 final class OperationsTest extends TestCase
 {
     public function testAdd():void {
-		$operations = new Operations();
+		$operations = new learninggit\Operations();
 		$this->assertEquals($_ENV['DEP_KEY'],"MY_DEPLOYMENT_KEY_123456");
 		$this->assertEquals(4,$operations->add(1,3));
 	}
