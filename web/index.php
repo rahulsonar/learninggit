de2c7c26fda7301848055c9661ef00577237dbb3
Rahul Sonar <sonar.soni@gmail.com>
2021-01-07 10:50:03 +0530

    fixes

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
-<?php 
+<?php
 
+/**
+ * Index.php
+ * php version 7.1
+ 
+ * @Description Entry point
+ * @category    File
+ * @package     Learninggit
+ * @author      My Name <my.name@example.com>
+ * @license     GPL
+ * @link        https://rahulsonar.com
+ */
+ 
 echo "Hello World!";
-
-?>
