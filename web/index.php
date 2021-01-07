32bd258fec132828a89923546053840ac068044f
Rahul Sonar <sonar.soni@gmail.com>
2021-01-07 10:52:41 +0530

    fixes

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
  * @category    File
  * @package     Learninggit
  * @author      My Name <my.name@example.com>
- * @license     GPL
+ * @license     http://www.php.net/license/3_01.txt  PHP License 3.01
  * @link        https://rahulsonar.com
  */
  
